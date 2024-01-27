12d153548ba6fadc13653c9197124d83e5bf6ec7
Maarten Engels <maartene@thedreamweb.eu>
2024-01-27 13:07:10 +0100

    Initial Commit

---
+++ IsoTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IsoTutorial
+//
+//  Created by Maarten Engels on 27/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IsoTutorial/IsoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsoTutorialApp.swift
+//  IsoTutorial
+//
+//  Created by Maarten Engels on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct IsoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
